6ec78b521ebd7e84ebadc5340a9401b54d711e3c
Matt Jarvis <matt.jarvis@astinc.us>
2018-01-03 20:40:36 -0500

    Wavein enumeration

--- FloatingBallGame/FloatingBallGame/MainWindow.xaml.cs
+++ FloatingBallGame/FloatingBallGame/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FloatingBallGame.ViewModels;
 
 namespace FloatingBallGame
 {
@@ -23,6 +24,7 @@ namespace FloatingBallGame
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = AppViewModel.Global;
         }
     }
 }
--- FloatingBallGame/FloatingBallGame/ViewModels/AppViewModel.cs
+++ FloatingBallGame/FloatingBallGame/ViewModels/AppViewModel.cs
@@ -1,7 +1,45 @@
-﻿namespace FloatingBallGame.ViewModels
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FloatingBallGame.Annotations;
+using NAudio.CoreAudioApi;
+using NAudio.Wave;
+
+namespace FloatingBallGame.ViewModels
 {
-    public class AppViewModel
+    public class AppViewModel : INotifyPropertyChanged
     {
-        
+        private static AppViewModel _global;
+
+        public static AppViewModel Global => _global ?? (_global = new AppViewModel());
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> DeviceNames { get; set; }
+
+        private AppViewModel()
+        {
+            this.DeviceNames = new ObservableCollection<string>();
+            int waveInDevices = WaveIn.DeviceCount;
+            for (int waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(waveInDevice);
+                this.DeviceNames.Add($"Device {waveInDevice}: {deviceInfo.ProductName}, {deviceInfo.Channels}");
+            }
+
+            return;
+            MMDeviceEnumerator enumerator = new MMDeviceEnumerator();
+            foreach (MMDevice device in enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.All))
+            {
+                this.DeviceNames.Add($"{device.FriendlyName}, {device.State}");
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
