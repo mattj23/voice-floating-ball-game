3c3c547f56934d169ec4864d14802f40b3aab660
Matt Jarvis <matt.jarvis@astinc.us>
2018-02-05 22:05:39 -0500

    Layout improvements

---
+++ FloatingBallGame/FloatingBallGame/ViewModels/CalibrationViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using FloatingBallGame.Annotations;
+using FloatingBallGame.Audio;
+using NAudio.Wave;
+
+namespace FloatingBallGame.ViewModels
+{
+    public class CalibrationViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public IGameWavePrecursor Precursor { get; private set; }
+        public IGameWaveProvider Provider { get; private set; }
+
+        public CalibrationViewModel()
+        {
+            
+        }
+
+        public void SetPrecursor(IGameWavePrecursor precursor)
+        {
+            // Unsubscribe
+            if (this.Provider != null)
+                this.Provider.DataAvailable -= ProviderOnDataAvailable;
+
+            this.Precursor = precursor;
+            this.Provider = precursor.ToProvider();
+            this.Provider.DataAvailable += ProviderOnDataAvailable;
+        }
+
+        private void ProviderOnDataAvailable(object sender, WaveInEventArgs waveInEventArgs)
+        {
+            throw new NotImplementedException();
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FloatingBallGame/FloatingBallGame/Views/CalibrationView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using FloatingBallGame.ViewModels;
+
+namespace FloatingBallGame.Views
+{
+    /// <summary>
+    /// Interaction logic for CalibrationView.xaml
+    /// </summary>
+    public partial class CalibrationView : UserControl
+    {
+        public static readonly DependencyProperty CalibrationProperty = DependencyProperty.Register(
+            "Calibration", typeof(CalibrationViewModel), typeof(CalibrationView), new PropertyMetadata(default(CalibrationViewModel)));
+
+        public CalibrationViewModel Calibration
+        {
+            get { return (CalibrationViewModel) GetValue(CalibrationProperty); }
+            set { SetValue(CalibrationProperty, value); }
+        }
+
+        public CalibrationView()
+        {
+            InitializeComponent();
+            this.LayoutRoot.DataContext = this;
+        }
+    }
+}
